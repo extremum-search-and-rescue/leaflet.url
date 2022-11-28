e0141e29d3e9af29ade520274baf70c9685222cc
Alexey Belkin <alexey.belkin@mail.ru>
2022-11-28 03:00:08 +0300

    add url parsing

--- leaflet.url.js
+++ leaflet.url.js
@@ -53,8 +53,54 @@ var L;
             const hash = this._getHash();
             return `${sitename}/#${hash}&p=${latLng.lat.toFixed(5)},${latLng.lng.toFixed(5)}`;
         }
+        static getState(lochash, baseMaps, overlayMaps, defaultMap, defaults) {
+            const state = new MapStateBase();
+            state.lat = defaults.lat;
+            state.lng = defaults.lng;
+            state.zoom = defaults.zoom;
+            state.baseLayerTheme = defaults.baseLayerTheme;
+            state.selectedLayers = [defaultMap];
+            const queryStringRegEx = lochash.match(/#z=(\d{1,2}[.0-9]*)&c=(-?\d{1,2}\.?\d{0,16}),(-?\d{1,3}\.?\d{0,16})&l=([\w/]*)(&p=(-?\d{1,2}\.?\d{0,16},-?\d{1,3}\.?\d{0,16}[/]?)+)?/);
+            if (queryStringRegEx) {
+                state.lat = parseFloat(queryStringRegEx[2]);
+                state.lng = parseFloat(queryStringRegEx[3]);
+                state.zoom = parseFloat(queryStringRegEx[1]);
+                const pointsPart = queryStringRegEx.length >= 6 ? queryStringRegEx[5] : undefined;
+                let proposedLayers = [];
+                queryStringRegEx[4]
+                    .split('/')
+                    .forEach(function (l) {
+                    const baseMap = Object.entries(baseMaps)
+                        .map(bm => bm)
+                        .filter(bm => bm[1].options.id === l);
+                    if (baseMap.length > 0)
+                        proposedLayers.push(baseMap[0][1]);
+                    const overlayMap = Object.entries(overlayMaps)
+                        .map(bm => bm)
+                        .filter(om => om[1] != null && om[1].options.id == l);
+                    if (overlayMap.length > 0)
+                        proposedLayers.push(overlayMap[0][1]);
+                });
+                if (proposedLayers.length > 0)
+                    state.selectedLayers = proposedLayers;
+                if (pointsPart) {
+                    const pointsRegEx = pointsPart.match(/(-?\d{1,2}\.?\d{0,16}),(-?\d{1,3}\.?\d{0,16})/);
+                    if (pointsRegEx && pointsRegEx.length === 3)
+                        state.highlightedPoints.push(L.latLng(parseFloat(pointsRegEx[1]), parseFloat(pointsRegEx[2])));
+                }
+            }
+            ;
+            state.baseLayerTheme = state.selectedLayers[0].options.theme;
+            return state;
+        }
     }
     L.UrlUpdater = UrlUpdater;
+    class MapStateBase {
+        constructor() {
+            this.highlightedPoints = [];
+        }
+    }
+    L.MapStateBase = MapStateBase;
     L.Map.mergeOptions({
         urlUpdater: false
     });
--- leaflet.url.ts
+++ leaflet.url.ts
@@ -47,7 +47,7 @@ namespace L {
             );
             this._events = null;
         }
-        private _getHash() : string {
+        private _getHash(): string {
             const mapCenter = this._map.wrapLatLng(this._map.getCenter());
             const z = this._map.getZoom().toPrecision(2);
             const cLat = mapCenter.lat.toFixed(4);
@@ -80,6 +80,73 @@ namespace L {
             const hash = this._getHash();
             return `${sitename}/#${hash}&p=${latLng.lat.toFixed(5)},${latLng.lng.toFixed(5)}`;
         }
+        static getState(
+            lochash: string,
+            baseMaps: LayerControlGroup,
+            overlayMaps: LayerControlGroup,
+            defaultMap: LayerControlEntry,
+            defaults
+        ): MapStateBase {
+            const state = new MapStateBase();
+
+            state.lat = defaults.lat;
+            state.lng = defaults.lng;
+            state.zoom = defaults.zoom;
+            state.baseLayerTheme = defaults.baseLayerTheme;
+            state.selectedLayers = [defaultMap];
+
+            const queryStringRegEx = lochash.match(/#z=(\d{1,2}[.0-9]*)&c=(-?\d{1,2}\.?\d{0,16}),(-?\d{1,3}\.?\d{0,16})&l=([\w/]*)(&p=(-?\d{1,2}\.?\d{0,16},-?\d{1,3}\.?\d{0,16}[/]?)+)?/);
+
+            if (queryStringRegEx) {
+                state.lat = parseFloat(queryStringRegEx[2]);
+                state.lng = parseFloat(queryStringRegEx[3]);
+                state.zoom = parseFloat(queryStringRegEx[1]);
+
+                const pointsPart = queryStringRegEx.length >= 6 ? queryStringRegEx[5] : undefined;
+                let proposedLayers: Array<LayerControlEntry> = [];
+                queryStringRegEx[4]
+                    .split('/')
+                    .forEach(function (l) {
+                        const baseMap = Object.entries(baseMaps)
+                            .map(bm => bm as [string, LayerControlEntry])
+                            .filter(bm => bm[1].options.id === l);
+                        if (baseMap.length > 0)
+                            proposedLayers.push(baseMap[0][1]);
+                        const overlayMap = Object.entries(overlayMaps)
+                            .map(bm => bm as [string, LayerControlEntry])
+                            .filter(om => om[1] != null && om[1].options.id == l);
+                        if (overlayMap.length > 0)
+                            proposedLayers.push(overlayMap[0][1]);
+                    });
+                if (proposedLayers.length > 0)
+                    state.selectedLayers = proposedLayers;
+                if (pointsPart) {
+                    const pointsRegEx = pointsPart.match(/(-?\d{1,2}\.?\d{0,16}),(-?\d{1,3}\.?\d{0,16})/);
+                    if (pointsRegEx && pointsRegEx.length === 3)
+                        state.highlightedPoints.push(L.latLng(
+                            parseFloat(pointsRegEx[1]),
+                            parseFloat(pointsRegEx[2]))
+                        );
+                }
+            };
+            state.baseLayerTheme = (state.selectedLayers[0] as TileLayer).options.theme;
+            return state;
+        }
+
+    }
+    type LayerControlEntry = LayerGroup<TileLayer> & { options: { id: string } } | TileLayer | GeoJsonLayer; 
+
+    interface LayerControlGroup {
+        [name: string]: LayerControlEntry
+    }
+    export class MapStateBase {
+        lat: number;
+        lng: number;
+        latlng: L.LatLng;
+        zoom: number;
+        selectedLayers: Array<LayerControlEntry>;
+        baseLayerTheme: string;
+        highlightedPoints?: Array<L.LatLng> = []
     }
     L.Map.mergeOptions({
         //disable updateUrlOnEvent by default
